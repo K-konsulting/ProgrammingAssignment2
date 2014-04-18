3c7836fc1fa8ae5fb60ab5a57810ec832013b426
K-konsulting <lars@k-konsulting.se>
2014-04-18 23:01:18 +0200

    Implemented functions and added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix wrapps a matrix into a list containing
+## methods that are used to store and retrieve not only
+## the matrix itself but also any addtional data, that
+## then become logically bounded to that matrix.
+##
+## cacheSolve utilizes this wrapper to avoid doing
+## repeated matrix inverse computations upon repeated
+## calls to this function with the same wrapped matrix.
 
-## Write a short comment describing this function
 
+## I decided to clean up the interface a little bit
+## and only provide two functions; one for retrieval 
+## and one for storing. If no 'name' parameter is
+## supplied, then the base matrix is automatically
+## returned or stored.
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache = list(matrix = x)
+  get = function (name = "matrix") cache[[name]]
+  set = function (value, name = "matrix") {
+    if (name == "matrix")
+      cache <<- list(matrix = value)
+    else
+      cache[[name]] <<- value
+  }
+  list(get = get, set = set)
 }
 
 
-## Write a short comment describing this function
-
+## Only performs the matrix inverse calculations if
+## no cached result is available, and stores the
+## computed result before it's being returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get("inverse")
+  if (is.null(inverse)) {
+    m <- x$get()
+    inverse <- solve(m, ...)
+    x$set(inverse, "inverse")
+  }
+  inverse
 }
